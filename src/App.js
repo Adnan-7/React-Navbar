002f9ea8a160ac37e7184664d0a59792b3b3ea9f
Adnan Khan <adnan7.zain@gmail.com>
2021-11-24 21:57:21 +0500

    navbar basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './navbar';
 
 function App() {
   return (
     <div>
-      App component
+      <Navbar />
     </div>
   );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import logo from './logo.svg';
 
 const Navbar = () => {
-    return (
-        <div>
-            Nabar component
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
         </div>
-    )
-}
+        <div className='links-container show-container'>
+          <ul className='links'>
+            <li>
+              <a href='#'>Home</a>
+            </li>
+            <li>
+              <a href='#'>about</a>
+            </li>
+            <li>
+              <a href='#'>projects</a>
+            </li>
+            <li>
+              <a href='#'>contact</a>
+            </li>
+            <li>
+              <a href='#'>profile</a>
+            </li>
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          <li>
+            <a href='www.twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='www.twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href='www.twitter.com'>
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
